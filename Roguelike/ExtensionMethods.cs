77ebc3b117a970f6e35a4b4cce479f3c066c80bf
Sung Ho Kim <likeleon@gmail.com>
2017-03-21 23:55:03 +0900

    시작

---
+++ Roguelike/ExtensionMethods.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Roguelike
+{
+    public static class ExtensionMethods
+    {
+        public static Vector2 ToVector2(this Point point) => new Vector2(point.X, point.Y);
+    }
+}
---
+++ Roguelike/RoguelikeGame.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Roguelike
+{
+    public class RoguelikeGame : Game
+    {
+        private readonly GraphicsDeviceManager _graphics;
+        private Vector2 _viewportCenter;
+        private Point _viewportSize;
+
+        private SpriteBatch _spriteBatch;
+        
+        public RoguelikeGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            _graphics.SynchronizeWithVerticalRetrace = true;
+
+            IsMouseVisible = false;
+            Window.Title = "Roguelike";
+        }
+
+        protected override void Initialize()
+        {
+            _viewportCenter = GraphicsDevice.Viewport.Bounds.Center.ToVector2();
+            _viewportSize = GraphicsDevice.Viewport.Bounds.Size;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
