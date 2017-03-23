e387de214404e5a374c0977da68a99ac66caf850
Sung Ho Kim <likeleon@gmail.com>
2017-03-23 23:32:41 +0900

    ScalingViewportAdapter

---
+++ Roguelike/Graphics/ViewportAdapter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Roguelike.Graphics
+{
+    public abstract class ViewportAdapter
+    {
+        protected ViewportAdapter(GraphicsDevice graphicsDevice)
+        {
+            GraphicsDevice = graphicsDevice;
+        }
+
+        public GraphicsDevice GraphicsDevice { get; }
+        public Viewport Viewport => GraphicsDevice.Viewport;
+
+        public abstract int VirtualWidth { get; }
+        public abstract int VirtualHeight { get; }
+        public abstract int ViewportWidth { get; }
+        public abstract int ViewportHeight { get; }
+
+        public Rectangle BoundingRectangle => new Rectangle(0, 0, VirtualWidth, VirtualHeight);
+        public Point Center => BoundingRectangle.Center;
+
+        public abstract Matrix GetScaleMatrix();
+
+        public virtual Point PointToScreen(Point point)
+        {
+            var invertedScaleMatrix = Matrix.Invert(GetScaleMatrix());
+            return Vector2.Transform(point.ToVector2(), invertedScaleMatrix).ToPoint();
+        }
+
+        public virtual void Reset()
+        {
+        }
+    }
+}
--- Roguelike/RoguelikeGame.cs
+++ Roguelike/RoguelikeGame.cs
@@ -9,8 +9,9 @@ namespace Roguelike
     public class RoguelikeGame : Game
     {
         private readonly GraphicsDeviceManager _graphics;
-        private Vector2 _viewportCenter;
-        private Point _viewportSize;
+        private ScalingViewportAdapter _scalingViewportAdapter;
+        private Vector2 _screenCenter;
+        private Point _screenSize;
 
         private SpriteBatch _spriteBatch;
         private SpriteFont _font;
@@ -22,8 +23,8 @@ namespace Roguelike
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
-            _graphics.PreferredBackBufferWidth = 1920;
-            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 800;
             _graphics.SynchronizeWithVerticalRetrace = true;
 
             IsMouseVisible = false;
@@ -32,8 +33,9 @@ namespace Roguelike
 
         protected override void Initialize()
         {
-            _viewportCenter = GraphicsDevice.Viewport.Bounds.Center.ToVector2();
-            _viewportSize = GraphicsDevice.Viewport.Bounds.Size;
+            _scalingViewportAdapter = new ScalingViewportAdapter(GraphicsDevice, 1920, 1080);
+            _screenCenter = _scalingViewportAdapter.BoundingRectangle.Center.ToVector2();
+            _screenSize = _scalingViewportAdapter.BoundingRectangle.Size;
 
             base.Initialize();
         }
@@ -73,19 +75,19 @@ namespace Roguelike
             // Coin and Gem
             _uiSprites.Add(new Sprite(Content.Load<Texture2D>("UI/spr_gem_ui"))
             {
-                Position = new Vector2(_viewportCenter.X - 260.0f, 50.0f),
+                Position = new Vector2(_screenCenter.X - 260.0f, 50.0f),
                 Origin = new Vector2(42.0f, 36.0f)
             });
             _uiSprites.Add(new Sprite(Content.Load<Texture2D>("UI/spr_coin_ui"))
             {
-                Position = new Vector2(_viewportCenter.X + 60.0f, 50.0f),
+                Position = new Vector2(_screenCenter.X + 60.0f, 50.0f),
                 Origin = new Vector2(48.0f, 24.0f)
             });
 
             // Key pickup
             _uiSprites.Add(new Sprite(Content.Load<Texture2D>("UI/spr_key_ui"))
             {
-                Position = _viewportSize.ToVector2() - new Vector2(120.0f, 70.0f),
+                Position = _screenSize.ToVector2() - new Vector2(120.0f, 70.0f),
                 Origin = new Vector2(90.0f, 45.0f),
                 Color = new Color(255, 255, 255, 60)
             });
@@ -94,31 +96,31 @@ namespace Roguelike
             _uiSprites.Add(new Sprite(Content.Load<Texture2D>("UI/spr_attack_ui"))
             {
                 Origin = new Vector2(16.0f, 16.0f),
-                Position = _viewportCenter - new Vector2(270.0f, 30.0f)
+                Position = _screenCenter - new Vector2(270.0f, 30.0f)
             });
 
             _uiSprites.Add(new Sprite(Content.Load<Texture2D>("UI/spr_defense_ui"))
             {
                 Origin = new Vector2(16.0f, 12.0f),
-                Position = _viewportCenter - new Vector2(150.0f, 30.0f)
+                Position = _screenCenter - new Vector2(150.0f, 30.0f)
             });
 
             _uiSprites.Add(new Sprite(Content.Load<Texture2D>("UI/spr_strength_ui"))
             {
                 Origin = new Vector2(22.0f, 12.0f),
-                Position = _viewportCenter - new Vector2(30.0f, 30.0f)
+                Position = _screenCenter - new Vector2(30.0f, 30.0f)
             });
 
             _uiSprites.Add(new Sprite(Content.Load<Texture2D>("UI/spr_dexterity_ui"))
             {
                 Origin = new Vector2(16.0f, 12.0f),
-                Position = _viewportCenter + new Vector2(90.0f, -30.0f)
+                Position = _screenCenter + new Vector2(90.0f, -30.0f)
             });
 
             _uiSprites.Add(new Sprite(Content.Load<Texture2D>("UI/spr_stamina_ui"))
             {
                 Origin = new Vector2(16.0f, 16.0f),
-                Position = _viewportCenter + new Vector2(210.0f, -30.0f)
+                Position = _screenCenter + new Vector2(210.0f, -30.0f)
             });
         }
 
@@ -141,11 +143,11 @@ namespace Roguelike
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            _spriteBatch.Begin();
+            _spriteBatch.Begin(transformMatrix: _scalingViewportAdapter.GetScaleMatrix());
 
             var text = "MonoGame Font Test";
             var textMiddlePoint = _font.MeasureString(text) / 2;
-            _spriteBatch.DrawString(_font, text, _viewportCenter, Color.White, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
+            _spriteBatch.DrawString(_font, text, _screenCenter, Color.White, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
 
             foreach (var sprite in _uiSprites)
                 sprite.Draw(_spriteBatch);
