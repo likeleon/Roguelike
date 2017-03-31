bb2b089d0dec9273dfe8cc26adccf69c12db11c7
Sung Ho Kim <likeleon@gmail.com>
2017-03-31 15:16:44 +0900

    Potion

---
+++ Roguelike/Objects/Items/Potion.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Roguelike.Objects
+{
+    public sealed class Potion : Item
+    {
+        public int Attack { get; }
+        public int Defense { get; }
+        public int Strength { get; }
+        public int Dexterity { get; }
+        public int Stamina { get; }
+
+        public Potion(ContentManager content)
+            : base(ItemType.Potion)
+        {
+            SetSprite(content.Load<Texture2D>("Loot/Potions/spr_potion_stamina"), 8, 12);
+        }
+    }
+}
