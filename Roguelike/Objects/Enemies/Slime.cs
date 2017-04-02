3446eed5dbf1fd67138f2141577fdbae420f1c21
Sung Ho Kim <likeleon@gmail.com>
2017-04-02 23:59:00 +0900

    슬라임 색, 크기 랜덤

--- Roguelike/Objects/Enemies/Slime.cs
+++ Roguelike/Objects/Enemies/Slime.cs
@@ -1,4 +1,6 @@
-﻿namespace Roguelike.Objects
+﻿using Microsoft.Xna.Framework;
+
+namespace Roguelike.Objects
 {
     public sealed class Slime : Enemy
     {
@@ -8,6 +10,20 @@
                 Textures[(int)kvp.Key] = kvp.Value;
 
             SetSprite(Textures[(int)(AnimationState.WalkUp)], frames: 8, frameSpeed: 12);
+
+            Sprite.Color = GetRandomColor();
+
+            var scale = Rand.Next(5, 15) / 10.0f;
+            Sprite.Scale = new Vector2(scale);
+        }
+
+        private static Color GetRandomColor()
+        {
+            return Color.FromNonPremultiplied(
+                r: Rand.Next(256),
+                g: Rand.Next(256),
+                b: Rand.Next(256),
+                a: Rand.Next(100, 256));
         }
     }
 }
